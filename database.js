7799bf23ee0b831bd5252414a369a17ed3086619
karthickraja-G <karthickrajasudhir.g@gmail.com>
2026-06-03 21:36:18 +0530

    next stage setup

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'hookGuard-db',
-    password: 'karthick',
-    port: 5432,
-})
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10),
+});
+module.exports = pool;
